b4b2fd9f97e409d9158f7d2aa7afd7b04556f471
cpaggen <cpaggen@cisco.com>
2020-05-09 12:58:56 +0200

    added remove example for PATCH

--- patchTemplate.py
+++ patchTemplate.py
@@ -13,22 +13,24 @@ except ImportError:
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 rc = mso.RestClient(MSO_IP, MSO_ADMIN, MSO_PASSWORD)
 templateName = 'Template1'
+schemaName = 'schema_one'
 
 # let's fetch the id of schema 'schema_one'
 schemas = rc.get('/schemas')
 schemasJson = json.loads(schemas.text)
 for schema in schemasJson['schemas']:
-    if schema['displayName'] == 'schema_one':
+    if schema['displayName'] == schemaName:
         schemaId = schema['id']
+        # for sake of simplicity we are retrieving the first site only
         siteId = schema['sites'][0]['siteId']
 
 print("Schema has id {} and site is {}".format(schemaId,siteId))
 
+# let's add a static path to an EPG in Template1
 path = '/sites/' + siteId + '-' + templateName + '/anps/ap_one/epgs/epg_one/staticPorts/-'
-
 patchSet = [{ "op": "add",
-             "path": path,
-             "value": {
+              "path": path,
+              "value": {
                             "type": "port",
                             "path": "topology/pod-1/paths-101/pathep-[eth1/17]",
                             "portEncapVlan": 51,
@@ -41,4 +43,21 @@ url = '/schemas/' + schemaId
 resp = rc.patch(url, json_body=patchSet)
 print("\n")
 pprint.pprint(json.loads(resp.text))
+delOp = input("Check MSO for new static path under EPG. Delete new path now? [Y/N]")
+if delOp == 'Y':
+    path = '/sites/' + siteId + '-' + templateName + '/anps/ap_one/epgs/epg_one/staticPorts/'
+    patchSet = [{ "op": "remove",
+                  "path": path,
+                  "value": {
+                            "type": "port",
+                            "path": "topology/pod-1/paths-101/pathep-[eth1/17]",
+                            "portEncapVlan": 51,
+                            "deploymentImmediacy": "immediate",
+                            "mode": "regular"
+                          }
+               }]
+
+    resp = rc.patch(url, json_body=patchSet)
+    pprint.pprint(json.loads(resp.text))
+    print("Path removed")
 
